323fc7425baecffca971af8c68156072eaaa2fd8
Piyush Kunjilwar <145169506+piyush12kunjilwar@users.noreply.github.com>
2025-04-25 02:03:34 -0400

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/piyushkunjilwar.github.io/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+});
